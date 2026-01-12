b3a302029afce1a10ab82a47e5cbd24f7fa8a592
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-12 09:30:57 -0700

    lists notes first

--- notes/lists.py
+++ notes/lists.py
@@ -1,2 +1,27 @@
 # VL 1st Types of Lists Notes
-#This is a change to make it so I can commit and push
+
+siblings = ["Alex", "Katie", "Andrew", "Tia", "Treyson", "Xavier", "Jake", "Katie"]
+
+print(siblings[3])
+siblings[-2] = "DOW"
+
+print(siblings)
+
+
+fruit = ("apple", "orange", "peach", "kiwi", "raspberry")
+home = (0,0)
+x,y = home
+
+#fruit[3] = "pineapple"
+print(x)
+
+#set
+colors = {"Orange", "Purple", "Green", "Blue", "Yellow", "Red", "Green"}
+colors.add("Pink")
+colors.remove("Purple")
+for i in colors:
+    if i == "Orange":
+        i = "Burgendy"
+    print(i)
+
+print(colors)
