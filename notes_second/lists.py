0295f9c7ffc4368eb906e079bb1e8e0e426bbc77
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-12 10:32:23 -0700

    Lists notes

---
+++ notes_second/lists.py
@@ -0,0 +1,31 @@
+# VL 2nd Lists Notes
+
+#Lists
+siblings = ["Alex", "Katie", "Andrew", "Tia", "Treyson", "Xavier", "Jake"]
+siblings[-1] = "DOW"
+siblings.append("Alex")
+print(siblings)
+# catagories, data storage, control randomization, 
+#list methods .append .pop .remove .sort len() .clear .copy .index .extend .reverse
+
+#Tuples
+white = (255, 255, 255)
+#white[1] = 0
+print(white[0])
+#unpack tuple
+red,green,blue = white
+print(red)
+
+# level info, start point, preset data, anything you don't want changed!
+# .count() .index()
+
+# Set
+fruits = {"apple", "orange", "banana", "pineapple", "pear", "grape", "orange"}
+
+fruits.remove("pineapple")
+fruits.add("raspberry")
+print(fruits)
+
+# character list, loot, passwords, randomization 
+
+# .clear .difference .copy .pop 
